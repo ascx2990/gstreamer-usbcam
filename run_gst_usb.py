60d1bcfe33eca30fc65c889ccce1762dbb9b89d8
ascx2990 <ascx2990@gmail.com>
2021-10-06 17:50:33 +0800

    first commit

---
+++ run_gst_usb.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+
+import sys
+sys.path.append('../')
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst
+from common.is_aarch_64 import is_aarch64
+from common.bus_call import bus_call
+
+def bus_call(bus, message, loop):
+    t = message.type
+    if t == Gst.MessageType.EOS:
+        sys.stdout.write("End-of-stream\n")
+        loop.quit()
+    elif t == Gst.MessageType.ERROR:
+        err, debug = message.parse_error()
+        sys.stderr.write("Error: %s: %s\n" % (err, debug))
+        loop.quit()
+    return True
+
+def main(args):
+    # Check input arguments
+    if len(args) != 2:
+        sys.stderr.write("usage: %s <v4l2-device-path>\n" % args[0])
+        sys.exit(1)
+
+    # Standard GStreamer initialization
+    GObject.threads_init()
+    Gst.init(None)
+
+    # Create gstreamer elements
+    # Create Pipeline element that will form a connection of other elements
+    print("Creating Pipeline \n ")
+    pipeline = Gst.Pipeline()
+
+    if not pipeline:
+        sys.stderr.write(" Unable to create Pipeline \n")
+
+    # Source element for reading from the file
+    print("Creating Source \n ")
+    source = Gst.ElementFactory.make("v4l2src", "usb-cam-source")
+    if not source:
+        sys.stderr.write(" Unable to create Source \n")
+
+    caps_v4l2src = Gst.ElementFactory.make("capsfilter", "v4l2src_caps")
+    if not caps_v4l2src:
+        sys.stderr.write(" Unable to create v4l2src capsfilter \n")
+
+
+    print("Creating Video Converter \n")
+
+    # videoconvert to make sure a superset of raw formats are supported
+    vidconvsrc = Gst.ElementFactory.make("videoconvert", "convertor_src1")
+    if not vidconvsrc:
+        sys.stderr.write(" Unable to create videoconvert \n")
+
+    
+
+    # Finally render the osd output
+    if is_aarch64():
+        transform = Gst.ElementFactory.make("nvegltransform", "nvegl-transform")
+
+    print("Creating EGLSink \n")
+    sink = Gst.ElementFactory.make("xvimagesink", "nvvideo-renderer")
+    if not sink:
+        sys.stderr.write(" Unable to create egl sink \n")
+
+    print("Playing cam %s " %args[1])
+    caps_v4l2src.set_property('caps', Gst.Caps.from_string("video/x-raw, framerate=30/1, width=640, height=480"))
+    
+    source.set_property('device', args[1])
+   
+    sink.set_property('sync', False)#同步( 0:越快越好 1: 同步 )。
+
+    print("Adding elements to Pipeline \n")
+    pipeline.add(source)
+    pipeline.add(caps_v4l2src)
+    pipeline.add(vidconvsrc)
+   
+    pipeline.add(sink)
+    
+    print("Linking elements in the Pipeline \n")
+    source.link(caps_v4l2src)
+    caps_v4l2src.link(vidconvsrc)
+    vidconvsrc.link(sink)
+    
+
+    # # create an event loop and feed gstreamer bus mesages to it
+    loop = GObject.MainLoop()
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect ("message", bus_call, loop)
+
+    # # Lets add probe to get informed of the meta data generated, we add probe to
+    # # the sink pad of the osd element, since by that time, the buffer would have
+    # # had got all the metadata.
+
+    # start play back and listen to events
+    print("Starting pipeline \n")
+    pipeline.set_state(Gst.State.PLAYING)
+    try:
+        loop.run()
+    except:
+        pass
+    # cleanup
+    pipeline.set_state(Gst.State.NULL)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
